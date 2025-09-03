1f825650eced7a0052d5e1a6702b95147ad3d8f3
Truella <turahsuleiman1212@gmail.com>
2025-09-03 10:07:37 +0100

    Made chnages to build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
-set -o errexit  # stop on error
+# Exit on error
+set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
-python manage.py migrate --noinput
+# --- Start of corrected Tailwind setup ---
+# Change directory to the static_src folder
+cd theme/static_src
+
+# Install Node.js dependencies from package.json
+npm install
+
+# Change back to the project root
+cd ../../
+
+# Run the Django Tailwind build command from the project root
 python manage.py tailwind build
-python manage.py collectstatic --noinput --clear
+
+# --- End of corrected Tailwind setup ---
+
+# Collect static files and run migrations
+python manage.py collectstatic --noinput
+python manage.py migrate
